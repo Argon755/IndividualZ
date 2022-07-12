060162b2fcc919f17067be2fe493fa9fc434b689
argon <dima.shatunov.0202@mail.ru>
2022-07-12 18:30:22 +0300

    Done

--- ZadBinar.py
+++ ZadBinar.py
@@ -12,3 +12,18 @@ print(a)
 # искомое число
 value = int(input())
  
+mid = len(a) // 2
+low = 0
+high = len(a) - 1
+ 
+while a[mid] != value and low <= high:
+    if value > a[mid]:
+        low = mid + 1
+    else:
+        high = mid - 1
+    mid = (low + high) // 2
+ 
+if low > high:
+    print("No value")
+else:
+    print("ID =", mid)
